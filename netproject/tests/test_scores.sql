c41add754a0480d3f48728d9ec5c32e3961fe0f9
Mimoune Najimi <mimoune.najimi@gmail.com>
2026-07-28 17:55:07 +0200

    Add a macro function to test null values in columns

--- netproject/tests/test_scores.sql
+++ netproject/tests/test_scores.sql
@@ -4,3 +4,18 @@ select film_id,
 from {{ref('fact_scores')}}    
 where score <=0 and
       score_timestamp IS NULL
+
+
+-- I could use the macro function if i want to test that a table columns are not null,
+-- like this : 
+-- {{check_valeurs_nulles(ref('dim_films'))}}
+--> This will genrate somthing like this : 
+--SELECT *
+--FROM dim_fiml
+--WHERE
+--    film_id IS NULL
+--    OR titre_de_film IS NULL
+--    OR genre_array IS NULL
+--    OR genre IS NULL
+
+
